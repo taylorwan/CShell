aabf226a31e8c06315b1cf439e2868659e46338b
taylor <taylor@splashthat.com>
2016-02-10 22:11:00 -0500

    remove trailing whitespace, among other refactors

--- shell.c
+++ shell.c
@@ -19,16 +19,15 @@
 // builtins
 const char* home = "home";
 const char* dotdot = "..";
-const char *ciao = "exit";
-const char *cd = "cd";
-const char *pwd = "pwd";
-const char *_wait = "wait";
+const char* ciao = "exit";
+const char* cd = "cd";
+const char* pwd = "pwd";
+const char* _wait = "wait";
 const char* history = "history";
 const char* bangbang = "!!";
 const char* bang = "!";
 
 // error
-const char error_message[30] = "An error has occurred\n";
 const int ERROR_CODE = -1;
 
 // sizing
@@ -40,7 +39,7 @@ const int CHAR_SPACE = 32;
 const int CHAR_NEWLINE = 13;
 const int CHAR_NULL = 0;
 
-// constants for stack
+// stack
 const int PATHSTACK_SIZE = 50;
 char* pathStack[50];
 char* copyPathStack[50];
@@ -52,7 +51,7 @@ char* historyArr[200];
 const int HISTORYARR_SIZE = 200;
 int historyCount = 0;
 
-// true/false
+// boolean
 typedef enum {true, false} bool;
 
 
@@ -68,9 +67,6 @@ typedef enum {true, false} bool;
 /** printHistory: prints the last 10 items in history
  */
 void printHistory() {
-
-	char out[SIZE];
-
 	int i = 10;
 	while (i > 0) {
 		if (historyCount >= i) {
@@ -243,6 +239,7 @@ void _fillPathStack() {
  *	- shiftString
  *	- stripSpaces
  *	- stripEndOfLine
+ *	- stripEndSpace
  *	- stripNewline
  *	- stripString
  *	- nextNonSpaceChar
@@ -342,6 +339,19 @@ void stripEndOfLine(char * c) {
 	c[len-1] = '\0';
 }
 
+/** stripEndSpace: removes whitespace from the end of a string
+ * @param c - String to strip
+ */
+void stripEndSpace(char * c) {
+	int len = strlen(c);
+	while(len > 1) {
+		char cur = c[len-1];
+		if (cur == ' ' || cur == '\n')
+			c[len-1] = '\0';
+		len--;
+	}
+}
+
 /** stripNewline: removes all new line characters from a string
  * @param c - String to strip
  */
@@ -361,7 +371,7 @@ void stripNewline(char * c) {
  */
 void stripString(char * c) {
 	stripSpaces(c);
-	stripNewline(c);
+	stripEndSpace(c);
 }
 
 /** nextNonSpaceChar: return the next non-space character
@@ -417,6 +427,8 @@ void _printArray(char ** c, int sz) {
  * @return ERROR_CODE (should be -1)
  */
 int throwError() {
+
+	char * error_message = "An error has occurred\n";
 	write(STDERR_FILENO, error_message, strlen(error_message));
 	return ERROR_CODE;
 }
@@ -590,48 +602,45 @@ int parse(char * input) {
 
 	/* checking for redirection */
 
-	int charCount = countChar(input, '>');
+	int arrCount = countChar(input, '>');
 
 	// if there's a redirection
-	if (charCount > 0) {
+	if (arrCount > 0) {
 
 		strcpy(util, input);
 		char * cmd;
 		char * out = NULL;
 		char * ptr = strtok(util, "> ");
-		int count = 0;
+		int args = 0;
 
 		while (ptr != NULL) {
 
 			// more than one '>'
-			if (charCount > 1) { return throwError(); }
+			if (arrCount > 1) { return throwError(); }
 			// first argument
-			else if (count == 0) { cmd = ptr; }
+			else if (args == 0) { cmd = ptr; }
 			// second argument
-			else if (count == 1) { out = ptr; }
+			else if (args == 1) { out = ptr; }
 			// more than two args
 			else { return throwError(); }
 
 			// increment
 			ptr = strtok(NULL, "> ");
-			count++;
+			args++;
 		}
 
 		// check for the correct # of matched arguments
 		// or the '>' comes after the two args
-		if ((charCount+1 != count) ||
+		if ((arrCount+1 != args) ||
 			(strlen(out) + strlen(cmd) < findFirst(input, '>'))) {
 			return throwError();
 		}
 
 		// set redirect for outfile
-		if (out != NULL) {
-			printf("not nulllllll nopeee\n");
-			int outfile = open(out, O_RDWR|O_CREAT|O_TRUNC, S_IXUSR|S_IXUSR|S_IRUSR);
-			savedSTDOUT = dup(STDOUT_FILENO);
-			dup2(outfile, STDOUT_FILENO);
-			close(outfile);
-		}
+		int outfile = open(out, O_RDWR|O_CREAT|O_TRUNC, S_IXUSR|S_IXUSR|S_IRUSR);
+		savedSTDOUT = dup(STDOUT_FILENO);
+		dup2(outfile, STDOUT_FILENO);
+		close(outfile);
 	}
 
 
@@ -707,7 +716,6 @@ int main(int argc, char * argv[]) {
 
 		// child process: runs bash
 		else if (fk == 0) {
-
 			int result = parse(input);
 			if (result == ERROR_CODE) {
 				continue;
